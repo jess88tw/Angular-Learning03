655350300ce2d232e49fdf569e78920a727e7888
jess88tw <jess88tw@yahoo.com.tw>
2023-03-01 18:30:06 +0800

    Udemy class

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+})
+export class ServerComponent {
+  // ---------------------------------------------
+  // String interpolation
+  // ---------------------------------------------
+
+  serverId = 10;
+  serverStatus = 'offline';
+
+  // 方法也可以用在 string interpolation
+  // getServerStatus() {
+  //   return this.serverStatus;
+  // }
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
+}
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  // template: `` 可以直接寫 html 在裡面
+  templateUrl: './servers.component.html',
+  // styles: `` 可以直接寫 style 在裡面
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent implements OnInit {
+  // ---------------------------------------------
+  // Propetry binding
+  // ---------------------------------------------
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  // ---------------------------------------------
+  // Event binding
+  // ---------------------------------------------
+  serverCreatStatus = 'No server was created!';
+  serverName = 'TestServer';
+
+  // ngIf
+  serverCreated = false;
+
+  onCreateServer() {
+    this.serverCreatStatus = 'Server was created! Name is ' + this.serverName;
+    this.serverCreated = true;
+  }
+
+  // $event binding
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  ngOnInit() {}
+}
